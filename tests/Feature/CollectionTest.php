b6ecfbc8cb0cb205f1a8f0833267b532f11a6ebb
Sofa <muhammadsofa52@yahoo.com>
2023-04-02 02:57:59 +0700

    Map To Groups

---
+++ tests/Feature/CollectionTest.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+use App\Data\Person;
+
+class CollectionTest extends TestCase
+{
+    public function testCreateCollection()
+    {
+        $collection = collect([1, 2, 3, 4]);
+        //index harus sama kalau tidak peduli dengan index yang penting isi sama pakai assertEqualsCanonicalizing
+        $this->assertEqualsCanonicalizing([1, 2, 3, 4], $collection->all());
+    }
+
+    public function testForEach()
+    {
+        $collection = collect([1, 2, 3, 4, 5, 6, 7, 8, 9]);
+        foreach($collection as $key => $value){
+            $this->assertEquals($key + 1, $value);
+        }
+    }
+
+    public function testCrud()
+    {
+        $collection = collect([]);
+        $collection->push(1, 2, 3);
+        $this->assertEqualsCanonicalizing([1, 2, 3], $collection->all());
+
+        $result = $collection->pop();
+        $this->assertEquals(3, $result);
+        $this->assertEqualsCanonicalizing([1, 2], $collection->all());
+    }
+
+    public function testMap()
+    {
+        $collection = collect([1, 2, 3]);
+        $result = $collection->map(function ($item) {
+            return $item * 2;
+        });
+        $this->assertEqualsCanonicalizing([2, 4, 6], $result->all());
+    }
+
+    public function testMapInto()
+    {
+        $collection = collect(["Sofa"]);
+        $result = $collection->mapInto(Person::class);
+        $this->assertEquals([new Person("Sofa")], $result->all());
+    }
+
+    public function testMapSpread()
+    {
+        $collection = collect([
+            ["Sofa", "Yuliansyah"],
+            ["Affandi", "Susanto"]
+        ]);
+        $result = $collection->mapSpread(function ($firstName, $lastName) {
+            $fullName = $firstName . ' ' . $lastName;
+            return new Person($fullName);
+        });
+
+        $this->assertEquals([
+            new Person("Sofa Yuliansyah"),
+            new Person("Affandi Susanto"),
+        ], $result->all());
+    }
+
+     public function testMapToGroups()
+    {
+        $collection = collect([
+            [
+                "name" => "Sofa",
+                "department" => "IT"
+            ],
+            [
+                "name" => "Tasya",
+                "department" => "IT"
+            ],
+            [
+                "name" => "Budi",
+                "department" => "HR"
+            ]
+        ]);
+
+        $result = $collection->mapToGroups(function ($person) {
+            return [
+                $person["department"] => $person["name"]
+            ];
+        });
+
+        $this->assertEquals([
+            "IT" => collect(["Sofa", "Tasya"]),
+            "HR" => collect(["Budi"])
+        ], $result->all());
+
+    }
+}
